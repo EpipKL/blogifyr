2b0f65b6e92cca0f77b8e9aec284ddf212a44067
Sergio <119548442+sergiorodriguezdev@users.noreply.github.com>
2023-07-27 20:58:36 -0600

    Added MongoDB connection code

---
+++ server/config/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongodb://localhost:27017
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/blogifyr", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+module.exports = mongoose.connection;
