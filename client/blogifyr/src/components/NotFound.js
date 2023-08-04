5321bf30887e1eb779aa12ef4ee0285f3a6974b2
Sergio <119548442+sergiorodriguezdev@users.noreply.github.com>
2023-08-04 17:49:33 -0600

    Added user profile.

---
+++ client/blogifyr/src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="bg-white h-screen w-screen">
+      <div className="flex justify-between items-center">
+        <h1 className="text-dark text-4xl font-bold">Page not found</h1>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
