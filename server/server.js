cd6061f4b7fc4be929ab0a005b817b10bb88262f
Sergio <119548442+sergiorodriguezdev@users.noreply.github.com>
2023-08-03 20:50:50 -0600

    Updated Express route

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/blogifyr/build")));
 }
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/blogifyr/build/index.html"));
 });
 
